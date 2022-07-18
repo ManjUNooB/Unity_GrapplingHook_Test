0e730c7f1ab9d06b95f42f33c50bf7cbdf0023cb
ManjUNooB <ryuzin20000901@gmail.com>
2022-07-19 05:08:56 +0900

    MiniFPSControllerをAssetStoreからDL。自作のPlayerスクリプトをどうにかできないか改良？中

--- GrapplingHook_Test/Assets/Scripts/PlayerMove.cs
+++ GrapplingHook_Test/Assets/Scripts/PlayerMove.cs
@@ -4,35 +4,31 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-	[SerializeField, Range(0.0f, 100.0f)]
-	float maxSpeed;
-	[SerializeField, Range(0.0f, 20.0f)]
-	float powerAmount;
-	
-	[Space]
+	[SerializeField]
+	private float playerSpeed = 5.0f;
 
-	[Header("KeyCode")]
-	[SerializeField] KeyCode m_Forward;
-	[SerializeField] KeyCode m_Back;
-	[SerializeField] KeyCode m_Left;
-	[SerializeField] KeyCode m_Right;
+	[Header("Running")]
+	[SerializeField] 
+	private bool canRun = true;
+	public bool IsRunning { get; private set; }
+	[SerializeField]
+	private float runSpeed = 10.0f;
+	public KeyCode runningKey = KeyCode.LeftShift;
 
+	private Rigidbody rigidbody;
 
-	private Rigidbody m_rb;
+	public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
 
 	// Start is called before the first frame update
 	void Awake()
 	{
-		m_rb = this.transform.GetComponent<Rigidbody>();
+		//	このスクリプトのアタッチされているオブジェクトのRigidbodyを取得する
+		rigidbody = this.gameObject.GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 	private void FixedUpdate()
 	{
-		if (Input.GetKeyDown(m_Forward))
-		{
-			if (m_rb.velocity.magnitude < maxSpeed)
-				m_rb.AddForce(0,0,powerAmount,ForceMode.Acceleration);
-		}
+
 	}
 }
